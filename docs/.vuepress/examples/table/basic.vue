513d3d52bc75fa5131672a41f82a5a7076cc929d
chendj89 <chendj89@hotmail.com>
2022-07-09 09:18:03 +0800

    326544

--- docs/.vuepress/examples/table/basic.vue
+++ docs/.vuepress/examples/table/basic.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <el-form>
+    <el-form :model="table" :rules="table.rules">
       <el-table ref="mainTable" :data="table.list" border tooltip-effect="dark" style="width: 100%" size="small"
         :header-cell-style="{ textAlign: 'center' }" :cell-style="{ textAlign: 'center' }" row-key="id"
         highlight-current-row v-loading="loading">
-        <el-table-column type="selection">
+        <el-table-column type="selection" width="50">
         </el-table-column>
+        <el-table-column type="index" width="50"></el-table-column>
         <el-table-column prop="name" label="姓名"></el-table-column>
         <el-table-column prop="date" label="日期" show-overflow-tooltip></el-table-column>
         <el-table-column prop="age" label="年纪" show-overflow-tooltip>
           <template slot-scope="scope">
-            <el-form-item label-width="0">
-              <el-input size="mini" v-model="scope.row.age"></el-input>
+            <el-form-item label-width="0" :prop="'list.' + scope.$index + '.age'" :rules="table.rules.age">
+              <el-input size="mini" v-model.number="scope.row.age" :placeholder="msg.message">
+              </el-input>
             </el-form-item>
           </template>
         </el-table-column>
@@ -29,7 +31,20 @@ const mainTable = ref(null);
 const loading = ref(true)
 const table = reactive({
   list: [],
-  total: 0
+  total: 0,
+  rules: {
+    age: [
+      {
+        required: true,
+        message: "不能为空",
+        trigger: "change"
+      }, {
+        min: 2,
+        max: 10,
+        message: "长度为3~10",
+        trigger: "change"
+      }]
+  },
 });
 ins.proxy.$http.get("/api/mock", {
   "list|5": [
@@ -46,7 +61,6 @@ ins.proxy.$http.get("/api/mock", {
   table.list = res.data.list;
   table.total = res.data.total;
   loading.value = false;
-  console.log(table);
 })
 </script>
 
--- docs/blog/elementui.md
+++ docs/blog/elementui.md
@@ -9,6 +9,15 @@ title: 哈哈哈1
 table/basic
 :::
 
+
+### 序号
+
+- 手动添加一个 `el-table-column`，设`type`属性为`index`即可
+
+```vue
+<el-table-column type="index" width="55"></el-table-column>
+```
+
 ### 多选
 
 - 手动添加一个 `el-table-column`，设`type`属性为`selection`即可
@@ -31,9 +40,25 @@ table/basic
 form/checkbox
 :::
 
+### 验证
 
+- `el-form` 绑定 `model`, `el-table`的数据必须来源于`mode`
 
+```html
+<el-form :model="form" :rules="rules">
+  <el-table :data="form.list"></el-table>
+</el-form>
+```
 
+- `el-form-item`绑定验证,`prop`需要特殊处理
+
+```html
+<template slot-scope="scope">
+  <el-form-item label-width="0" :prop="'list.' + scope.$index + '.age'" :rules="form.rules.age">
+    <el-input size="mini" v-model="scope.row.age"></el-input>
+  </el-form-item>
+</template>
+```
 
 <!-- 脚本 -->
 <script setup>
