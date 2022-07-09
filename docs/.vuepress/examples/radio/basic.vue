4f0dbc012b6a5de8cd494f319f55ef5414cd379f
chendj89 <chendj89@hotmail.com>
2022-07-09 10:40:38 +0800

    43465

--- docs/.vuepress/examples/radio/basic.vue
+++ docs/.vuepress/examples/radio/basic.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
-    <el-radio-group v-model="radio" size="small" fill="#409EFF">
-      <el-radio-button
-        v-for="(item, index) in citys"
-        :key="index"
-        :label="item"
-      ></el-radio-button>
+    <el-radio-group v-model="radio" size="small" @change="change">
+      <el-radio-button v-for="(item, index) in citys" :key="index" :label="item">
+      </el-radio-button>
     </el-radio-group>
   </div>
 </template>
@@ -14,6 +11,9 @@
 import { ref } from "vue";
 const radio = ref("上海");
 const citys = ["上海", "北京", "广州", "深圳"];
+const change = () => {
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
