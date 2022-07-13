2839c5e6bd48f34a30acd681b52c1213c3b7a9b9
chendj89 <chendj89@hotmail.com>
2022-07-13 14:40:43 +0800

    435231

---
+++ docs/.vuepress/examples/input/number.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-input v-model.number="form.input"></el-input>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const form = reactive({
+  input: "",
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ docs/.vuepress/examples/input/number2.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-input
+    v-model="form.input"
+    @input="
+      form.input = form.input
+        .replace(/[^\d\.]/g, '')
+        .replace(/^\./g, '')
+        .replace(/\.{2}/, '.')
+        .replace('.', '$#$')
+        .replace(/\./g, '')
+        .replace('$#$', '.')
+    "
+  ></el-input>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const form = reactive({
+  input: 0,
+});
+</script>
+
+<style lang="scss" scoped></style>
