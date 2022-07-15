8117d44d118bc017d1f45a1eb4659c7eb24d4d14
chendj89 <chendj89@hotmail.com>
2022-07-15 20:58:24 +0800

    4422

--- cli/index.js
+++ cli/index.js
@@ -2,13 +2,9 @@ const fs = require("fs");
 
 function run() {
   try {
-    fs.writeFileSync(
-      "./docs/.vuepress/dist/readme.md",
-      new Date().toLocaleString(),
-      () => {
-        console.log(`更新完成`);
-      }
-    );
+    fs.writeFileSync("./readme.md", new Date().toLocaleString(), () => {
+      console.log(`更新完成`);
+    });
   } catch (error) {
     console.log(error);
   }
