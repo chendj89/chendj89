0571253c82fd45c9ff271b80109996caa1448376
chendj89 <chendj89@hotmail.com>
2022-07-15 21:14:35 +0800

    994

--- cli/index.js
+++ cli/index.js
@@ -5,6 +5,8 @@ function run() {
     fs.writeFileSync("./readme.md", new Date().toLocaleString(), () => {
       console.log(`更新完成`);
     });
+    let str = fs.readFileSync("./readme.md", "utf-8");
+    console.log(str);
   } catch (error) {
     console.log(error);
   }
