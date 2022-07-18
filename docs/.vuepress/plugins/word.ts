86934ad73437ff829b098523329ff70df428f7cd
chendj89 <chendj89@hotmail.com>
2022-07-18 19:50:23 +0800

    3456

--- docs/.vuepress/plugins/word.ts
+++ docs/.vuepress/plugins/word.ts
@@ -65,6 +65,7 @@ export const unplugin = createUnplugin(
         return id.endsWith(".md");
       },
       async transform(code) {
+        console.log('插件');
         let opts = Object.assign(
           {
             // 1 英式，2 美式
