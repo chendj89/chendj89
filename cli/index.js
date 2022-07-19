c0a74d567d60ddef6d18e03a743ca36b19a3372e
chendj89 <chendj89@hotmail.com>
2022-07-19 13:25:25 +0800

    655436

--- cli/index.js
+++ cli/index.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
 function run() {
+  let date = new Date();
+  date.setHours(date.getHours() + 8);
+  let updateTime= date.toLocaleString();
   let content = `
-  更新时间：${new Date().toLocaleString()}\n\t
+  更新时间：${updateTime}\n\t
   ![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=chendj89&theme=gruvbox&show_icons=true)`;
-
   try {
     // "./docs/.vuepress/dist/readme.md",
     fs.writeFileSync("./readme.md", content, () => {
