9789c8f2ecc6881486d9152cf9ac311285e00771
chendj89 <chendj89@hotmail.com>
2022-08-21 21:05:18 +0800

    453243

---
+++ docs/.vuepress/examples/vue/version.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="(attr, value) in version" :key="attr">
+      {{ attr }}：{{ value }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, getCurrentInstance } from "vue";
+let ins = getCurrentInstance();
+let version = reactive({
+  vue: ins.proxy.$root.$options._base.version,
+});
+</script>
+
+<style lang="scss" scoped></style>
